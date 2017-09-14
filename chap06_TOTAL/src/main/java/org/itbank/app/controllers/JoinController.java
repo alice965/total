66693dbd467e9c0c711e2f604fdb23d64492f136
1103-18 <1103-18@1103-18-PC>
2017-09-14 11:33:08 +0900

    설정포함

---
+++ chap06_TOTAL/src/main/java/org/itbank/app/controllers/JoinController.java
@@ -0,0 +1,45 @@
+package org.itbank.app.controllers;
+
+import java.sql.SQLException;
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.itbank.app.models.MemberDaoMyBatis;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class JoinController {
+	@Autowired
+	MemberDaoMyBatis memberDao;
+
+	@GetMapping(path = "/join")
+	public String joinGetHandle() {
+		return "t_join";
+	}
+
+	@PostMapping("/join")
+	public String joinPostHandle(@RequestParam Map map, HttpSession session, Model model) {
+		try {
+			boolean b = memberDao.addOne(map);
+			session.setAttribute("auth", "on");
+			return "redirect:/";
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("temp", map);
+			//model.addAttribute("section", "join");
+			return "join";
+		}
+	}
+
+	@PostMapping("/signup_check/{mode}")
+	public String signupHandle() {
+		return "";
+	}
+}
